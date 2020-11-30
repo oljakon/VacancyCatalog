47efd55fa081b309ba98d55d5bda00ddb843d8e4
oljakon <41132114+oljakon@users.noreply.github.com>
2020-11-30 03:09:26 +0300

    change urls

--- jobsite/jobsite/urls.py
+++ jobsite/jobsite/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 from django.urls import include
 from django.views.generic import RedirectView
 from catalog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.IndexView.as_view(), name="index"),
     path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', views.RegisterView.as_view(), name='register'),
 ]
